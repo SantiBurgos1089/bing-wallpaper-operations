deebfab199964de0f9011f69ef679175fcb4c8a5
Santiago Burgos <santiago.burgos1089@gmail.com>
2026-07-31 15:32:17 -0600

    added additional variables required for either Bing and Spotlight classes for downloading the picture

--- usr/lib/bingoper/bingoper.py
+++ usr/lib/bingoper/bingoper.py
@@ -1,6 +1,21 @@
 import requests
+import json
 from io import BytesIO
 from PIL import Image
+
+# The available markets that Bing actively serves localized wallpapers for.
+# "en-ROW" stands for Rest of World.
+BING_MARKET_CODES = [
+    "en-US", "en-GB", "en-CA", "en-AU", "en-NZ", "en-IN", "en-ROW",
+    "zh-CN", "ja-JP", "de-DE", "fr-FR", "fr-CA", "it-IT", "es-ES", "pt-BR"
+]
+
+# The available resolutions. "UHD" will pull down the 4K version (3840x2160).
+BING_RESOLUTIONS = [
+    "UHD", "1920x1200", "1920x1080", "1366x768", "1280x768",
+    "1024x768", "800x600", "800x480", "1080x1920", "768x1280", 
+    "720x1280", "640x480", "480x800", "400x240", "320x240", "240x320"
+]
 
 class WallpaperProvider:
     pass
