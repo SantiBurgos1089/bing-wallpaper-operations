c66f7af5b30656adde16236132292411966ace13
Santiago Burgos <santiago.burgos1089@gmail.com>
2026-07-31 11:35:37 -0600

    added a sample Bing operation class to take as template

---
+++ usr/lib/bingoper/bingoper.py
@@ -0,0 +1,12 @@
+import requests
+from io import BytesIO
+from PIL import Image
+
+class WallpaperProvider:
+    pass
+
+class BingProvider(WallpaperProvider):
+    pass
+
+class SpotlightProvider(WallpaperProvider):
+    pass
